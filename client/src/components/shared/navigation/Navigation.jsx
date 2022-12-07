6ba9efc261cbfe64286d9363453f2d5472e9a9c7
Sefatullah Omar <sefaullahomar1@gmail.com>
2022-12-06 23:17:17 -0800

    changed room styles

--- client/src/components/shared/navigation/Navigation.jsx
+++ client/src/components/shared/navigation/Navigation.jsx
@@ -4,7 +4,7 @@ import { logout } from '../../../http';
 import styles from './navigation.module.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { setAuth } from '../../../features/authSlice';
-
+import { RiLogoutCircleRLine } from 'react-icons/ri';
 
 
 
@@ -12,6 +12,7 @@ const Navigation = () => {
 
     const dispatch = useDispatch();
     const { isAuth, user } = useSelector((state) => state.auth);
+
     async function logoutUser() {
         try {
             const { data } = await logout();
@@ -29,12 +30,12 @@ const Navigation = () => {
                 <img 
                     className={styles.logoAvatar} 
                     src="https://cdn-icons-png.flaticon.com/512/2408/2408462.png" alt="logo" />
-                <span className={styles.logoText}>Codershouse</span>
+                <span className={styles.logoText}>CodersHouse</span>
             </Link>
 
             {isAuth && (
                 <div className={styles.navRight}>
-                    <h3>{user?.name}</h3>
+                    {/* <h3>{user?.name}</h3> */}
                     <Link to="/">
                         <img
                             className={styles.avatar}
@@ -51,7 +52,7 @@ const Navigation = () => {
                         className={styles.logoutButton}
                         onClick={logoutUser}
                     >
-                        <img src="/images/logout.png" alt="logout" />
+                        <RiLogoutCircleRLine />
                     </button>
                 </div>
             )}
--- client/src/pages/room/Room.jsx
+++ client/src/pages/room/Room.jsx
@@ -7,8 +7,8 @@ import styles from './room.module.css';
 
 
 function Room() {
-    const user = useSelector((state) => state.auth.user);
 
+    const user = useSelector((state) => state.auth.user);
     const [isMuted, setMuted] = useState(true);
     const [room, setRoom] = useState(null);
     
@@ -48,24 +48,21 @@ function Room() {
     };
 
     return (
-        <div>
+    <div className={styles.topRoom}>
 
-        <div className="container">
-            <button 
-            onClick={handManualLeave} 
-            className={styles.goBack}>
-                <img src="/images/arrow-left.png" alt="arrow-left" />
-                <span>All voice rooms</span>
-            </button>
-        </div>
+        <div className={styles.room}>
+            <div>
+                <button 
+                onClick={handManualLeave} 
+                className={styles.goBack}>
+                    <img src="/images/arrow-left.png" alt="arrow-left" />
+                    {room && <span className={styles.topic}>{room.topic}</span>}
+                </button>
+            </div>
 
-        <div className={styles.clientsWrap}>
-            <div className={styles.header}>
-                {room && <h2 className={styles.topic}>{room.topic}</h2>}
-                <div className={styles.actions}>
-                    <button className={styles.actionBtn}>
-                        <img src="/images/palm.png" alt="palm-icon" />
-                    </button>
+            <div className={styles.clientsWrap}>
+
+                <div className={styles.header}>
                     <button
                         onClick={handManualLeave}
                         className={styles.actionBtn}
@@ -74,50 +71,51 @@ function Room() {
                         <span>Leave quietly</span>
                     </button>
                 </div>
-            </div>
 
-            <div className={styles.clientsList}>
-                {clients.map((client) => {
-                    return (
-                        <div className={styles.client} key={client.id}>
-                            <div className={styles.userHead}>
-                                <img
-                                    className={styles.userAvatar}
-                                    src={client.avatar}
-                                    alt=""
-                                />
-                                <audio
-                                    autoPlay
-                                    ref={(instance) => {
-                                        provideRef(instance, client.id);
-                                    }}
-                                />
-                                <button
-                                    onClick={() =>handleMuteClick(client.id)}
-                                    className={styles.micBtn}
-                                >
-                                    {client.muted ? (
-                                        <img
-                                            className={styles.mic}
-                                            src="/images/mic-mute.png"
-                                            alt="mic"
-                                        />
-                                    ) : (
-                                        <img
-                                            className={styles.micImg}
-                                            src="/images/mic.png"
-                                            alt="mic"
-                                        />
-                                    )}
-                                </button>
+                <div className={styles.clientsList}>
+                    {clients.map((client) => {
+                        return (
+                            <div className={styles.client} key={client.id}>
+                                <div className={styles.userHead}>
+                                    <img
+                                        className={styles.userAvatar}
+                                        src={client.avatar}
+                                        alt=""
+                                    />
+                                    <audio
+                                        autoPlay
+                                        ref={(instance) => {
+                                            provideRef(instance, client.id);
+                                        }}
+                                    />
+                                    <button
+                                        onClick={() =>handleMuteClick(client.id)}
+                                        className={styles.micBtn}
+                                    >
+                                        {client.muted ? (
+                                            <img
+                                                className={styles.mic}
+                                                src="/images/mic-mute.png"
+                                                alt="mic"
+                                            />
+                                        ) : (
+                                            <img
+                                                className={styles.micImg}
+                                                src="/images/mic.png"
+                                                alt="mic"
+                                            />
+                                        )}
+                                    </button>
+                                </div>
+                                <h4>{client.name}</h4>
                             </div>
-                            <h4>{client.name}</h4>
-                        </div>
-                    );
-                })}
+                        );
+                    })}
+                </div>
+
             </div>
-            
         </div>
+
     </div>
     );
 };
